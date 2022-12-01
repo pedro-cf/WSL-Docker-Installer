847f3f6546ac4ee66759760f18f8dde46235732d
pedro-cf <pedro_fernandes@live.com>
2022-12-01 13:55:05 +0000

    first commit

---
+++ wsl2_install_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Please shutdown your VPN. Press any key to proceed..."
+read 
+sudo apt update -y && sudo apt upgrade -y
+sudo apt --fix-broken install -y
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo apt-get update
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --batch --yes
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s --force /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/dockerd" | sudo EDITOR='tee -a' visudo
+echo "$USER ALL=(root) NOPASSWD: /usr/sbin/service docker *" | sudo EDITOR='tee -a' visudo
+echo "sudo service docker status || sudo service docker start" >> ~/.profile
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo service docker status || sudo service docker start
